7a86016310cb0c70619f1e7af33889ee00b5a1c8
Vidit Khandelwal <vidit@gkmit.co>
2025-12-16 12:24:44 +0530

    fix: env file and config file

--- config/config.js
+++ config/config.js
@@ -5,6 +5,7 @@ module.exports = {
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+  host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT,
   define: {
     underscored: true,
